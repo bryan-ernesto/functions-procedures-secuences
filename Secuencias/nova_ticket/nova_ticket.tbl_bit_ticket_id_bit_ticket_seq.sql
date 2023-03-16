6064a50d374e18b476777e68b0533606ac689790
Bryan Gamez <bgamez@groupnova.com.gt>
2023-03-16 09:17:56 -0600

    add nova_ticket sequences

---
+++ Secuencias/nova_ticket/nova_ticket.tbl_bit_ticket_id_bit_ticket_seq.sql
@@ -0,0 +1,15 @@
+/*
+ Creado por: Bryan Gámez (bgamez@groupnova.com.gt)
+ Modificado por: Bryan Gámez (bgamez@groupnova.com.gt)
+ Creado: 13 marzo 2023
+ Modificado: 13 marzo 2023
+ Descripción: 
+ Dependencias: 
+ ERD:
+ Nombre ERD: 
+ */
+-- SEQUENCE: nova_ticket.tbl_bit_ticket_id_bit_ticket_seq
+-- DROP SEQUENCE IF EXISTS nova_ticket.tbl_bit_ticket_id_bit_ticket_seq;
+CREATE SEQUENCE IF NOT EXISTS nova_ticket.tbl_bit_ticket_id_bit_ticket_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+ALTER SEQUENCE nova_ticket.tbl_bit_ticket_id_bit_ticket_seq OWNER TO vince;
---
+++ Secuencias/nova_ticket/nova_ticket.tbl_cat_ticket_accion_id_cat_ticket_accion_seq.sql
@@ -0,0 +1,15 @@
+/*
+ Creado por: Bryan Gámez (bgamez@groupnova.com.gt)
+ Modificado por: Bryan Gámez (bgamez@groupnova.com.gt)
+ Creado: 13 marzo 2023
+ Modificado: 13 marzo 2023
+ Descripción: 
+ Dependencias: 
+ ERD:
+ Nombre ERD: 
+ */
+-- SEQUENCE: nova_ticket.tbl_cat_ticket_accion_id_cat_ticket_accion_seq
+-- DROP SEQUENCE IF EXISTS nova_ticket.tbl_cat_ticket_accion_id_cat_ticket_accion_seq;
+CREATE SEQUENCE IF NOT EXISTS nova_ticket.tbl_cat_ticket_accion_id_cat_ticket_accion_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+ALTER SEQUENCE nova_ticket.tbl_cat_ticket_accion_id_cat_ticket_accion_seq OWNER TO vince;
